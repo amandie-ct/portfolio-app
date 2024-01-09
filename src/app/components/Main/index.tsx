8ad61b16f4f34a40c7d8e27f0fd1051b3c3893f0
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-01-09 18:45:27 -0300

    first attempt at adding metadata and title

--- src/app/components/Main/index.tsx
+++ src/app/components/Main/index.tsx
@@ -1,9 +1,16 @@
 import About from '../About'
 import Footer from '../Footer'
 
+import { Metadata } from 'next'
+
 import LandingPage from '../LandingPage'
 import Projects from '../Projects'
 import * as Styled from './styles'
+// either Static metadata
+
+export const metadata: Metadata = {
+  title: 'Portfolio || Amanda Carvalho',
+}
 
 const Main = () => {
   return (
