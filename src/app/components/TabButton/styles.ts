52496970cc631e0a97e2bc2314e619f881948f01
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-01-09 18:01:58 -0300

    transforming props into transient props to fix warning

--- src/app/components/TabButton/styles.ts
+++ src/app/components/TabButton/styles.ts
@@ -1,14 +1,13 @@
-// 'use client'
 import styled, { css } from 'styled-components';
-
+import { ITabButtonProps } from '.';
 
 export interface TabButtonStyleProps {
   active?: boolean;
 }
 
-export const TabButton = styled.div<TabButtonStyleProps>`
-  ${(props) =>
-    props.active &&
+export const TabButton = styled.div<ITabButtonProps>`
+  ${($props) =>
+    $props.active &&
     css`
       border-bottom: 2px solid #d8a47a;
       color: #fff;
@@ -20,24 +19,3 @@ export const TabButton = styled.div<TabButtonStyleProps>`
   text-decoration: none;
   cursor: pointer;
 `;
-
-
-
-// import { ITabButtonProps } from '.';export interface TabButtonStyleProps {
-//   active?: string
-// }
-
-// export const TabButton = styled.div<TabButtonStyleProps>`
-//   ${(props) =>
-//     props.active &&
-//     css`
-//       border-bottom: 2px solid #d8a47a;
-//       color: #fff;
-//       font-weight: 500;
-//     `}
-
-//   margin-right: 4rem;
-//   padding-bottom: 0.5rem;
-//   text-decoration: none;
-//   cursor: pointer;
-// `;
