a298ad9e14cfef960a84488002f5dbcdfcfce569
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-03-27 21:47:08 -0300

    updating resume link

--- src/app/data/links.ts
+++ src/app/data/links.ts
@@ -1,2 +1,2 @@
 export const resumeLink =
-  'https://docs.google.com/document/d/1JIlbJ69chBAoZu03XcuZnIbkc_jK9U_F0sU-JlBU5sI/edit?usp=sharing'
+  'https://docs.google.com/document/d/1OLC_LhaHH_RvorkuINiIOG6O__dp7OgDB8-3YoZ1zFc/edit?usp=sharing'
